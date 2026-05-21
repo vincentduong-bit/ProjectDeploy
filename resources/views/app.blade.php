c07a1cb54471faffdae73e3d9fb17ad082593e77
unknown <dat.duong@efe.com.vn>
2026-05-22 00:27:43 +0700

    fix inertia vite production

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,8 +10,7 @@
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         @routes
-        @viteReactRefresh
-        @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
+        @vite(['resources/js/app.tsx'])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
