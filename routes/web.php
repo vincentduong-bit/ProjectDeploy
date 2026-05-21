607b3e02776075d5361dc5a7ea0800be51e23eb9
unknown <dat.duong@efe.com.vn>
2026-05-22 00:21:40 +0700

    fix home route response to return Hello World

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
+    echo "Hello World";
 })->name('home');
 
 Route::middleware(['auth'])->group(function () {
